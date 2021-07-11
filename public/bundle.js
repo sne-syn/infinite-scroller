988c71df953ba91736642acc36eec89c1c6d1c01
sne-syn <snezhana.syngur@gmail.com>
2021-07-11 14:05:03 +0200

    Add infinite load functionality

--- public/bundle.js
+++ public/bundle.js
@@ -3,32 +3,78 @@ var __webpack_exports__ = {};
 /*!*********************!*\
   !*** ./src/main.js ***!
   \*********************/
-const PHOTO_COUNT = 10;
+let count = 5;
 const API_KEY = "0sxPS6oS9ZwnBroHlHoq2XZJX_4Rjt7a5aVMdSD1gcc";
-const API_URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${PHOTO_COUNT}`;
+const API_URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${count}`;
 
-const loadPhotos = async () => {
-  try {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    fillImages(data);
-  } catch (error) {
-    // Catch Error Here
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+const setAttributes = (element, attributes) => {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
   }
 }
 
-const fillImages = (data) => {
+const imageLoaded = () => {
+  const loader = document.querySelector('.loader');
+  imagesLoaded++;
+  ready = imagesLoaded === totalImages ? true : false;
+  loader.hidden = true;
+  count = 30;
+};
+
+const displayPhotos = () => {
   const imageContainer = document.querySelector('.image-container');
-  data.forEach(unit => {
+
+  totalImages = photosArray.length;
+  imagesLoaded = 0;
+
+  photosArray.forEach(photo => {
+    // Create <a> to link to Unsplash
+    const imageLink = document.createElement('a');
+    setAttributes(imageLink, {
+      href: photo.urls.regular,
+      target: '_blank',
+      rel: 'noreferrer'
+    })
+
+    // Create <img> for photo
     const image = document.createElement('img');
-    image.src = unit.urls.raw;
-    image.alt = unit.alt_description;
+    setAttributes(image, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description
+    })
 
-    imageContainer.appendChild(image);
+    image.addEventListener('load', imageLoaded);
+    // Append created elements to the DOM
+    imageLink.appendChild(image);
+    imageContainer.appendChild(imageLink);
   });
-}
+};
 
-loadPhotos()
+const getPhotos = async () => {
+  try {
+    const response = await fetch(API_URL);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    // Catch Error Here
+  }
+};
+
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+getPhotos();
+
 
 /******/ })()
 ;
--- src/main.js
+++ src/main.js
@@ -1,26 +1,70 @@
-const PHOTO_COUNT = 10;
+let count = 5;
 const API_KEY = process.env.ACCESS_KEY;
-const API_URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${PHOTO_COUNT}`;
+const API_URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${count}`;
 
-const loadPhotos = async () => {
-  try {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    fillImages(data);
-  } catch (error) {
-    // Catch Error Here
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+const setAttributes = (element, attributes) => {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
   }
 }
 
-const fillImages = (data) => {
+const imageLoaded = () => {
+  const loader = document.querySelector('.loader');
+  imagesLoaded++;
+  ready = imagesLoaded === totalImages ? true : false;
+  loader.hidden = true;
+};
+
+const displayPhotos = () => {
   const imageContainer = document.querySelector('.image-container');
-  data.forEach(unit => {
+  totalImages = photosArray.length;
+  imagesLoaded = 0;
+
+  photosArray.forEach(photo => {
+    // Create <a> to link to Unsplash
+    const imageLink = document.createElement('a');
+    setAttributes(imageLink, {
+      href: photo.urls.regular,
+      target: '_blank',
+      rel: 'noreferrer'
+    })
+
+    // Create <img> for photo
     const image = document.createElement('img');
-    image.src = unit.urls.raw;
-    image.alt = unit.alt_description;
+    setAttributes(image, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description
+    })
 
-    imageContainer.appendChild(image);
+    image.addEventListener('load', imageLoaded);
+    // Append created elements to the DOM
+    imageLink.appendChild(image);
+    imageContainer.appendChild(imageLink);
   });
-}
+};
 
-loadPhotos()
+const getPhotos = async () => {
+  try {
+    const response = await fetch(API_URL);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    // Catch Error Here
+  }
+};
+
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+getPhotos();
+
